5a6af744132415c4905fbf082936c686465209e3
Alex Gleason <alex@alexgleason.me>
2023-03-24 20:17:51 -0500

    Add comments to types, deno fmt

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,15 +1,20 @@
 import { InputMessage, OutputMessage, Policy } from './types.ts';
 
+/** A policy function with opts to run it with. Used by the pipeline. */
 type PolicyTuple<Opts = unknown> = [policy: Policy<Opts>, opts?: Opts];
 
+/** Helper type for proper type inference of PolicyTuples in the pipeline. */
 // https://stackoverflow.com/a/75806165
 // https://stackoverflow.com/a/54608401
 type PolicyTuplesRest<T extends PolicyTuple[]> = {
-  [K in keyof T]: PolicyTuple<T[K]> | Policy<T[K]>
-}
+  [K in keyof T]: PolicyTuple<T[K]> | Policy<T[K]>;
+};
 
 /** Processes messages through multiple policies, bailing early on rejection. */
-async function pipeline<P extends any[]>(msg: InputMessage, policies: [...PolicyTuplesRest<P>]): Promise<OutputMessage> {
+async function pipeline<P extends any[]>(
+  msg: InputMessage,
+  policies: [...PolicyTuplesRest<P>],
+): Promise<OutputMessage> {
   for (const item of policies) {
     const [policy, opts] = toTuple(item);
     const result = await policy(msg, opts);
