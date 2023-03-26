a809da4249fa4eb545f9aa04342f83bd4471bccd
Alex Gleason <alex@alexgleason.me>
2023-03-26 16:19:33 -0500

    Add test for antiDuplicationPolicy

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export { readLines } from 'https://deno.land/std@0.181.0/io/mod.ts';
-export { assert } from 'https://deno.land/std@0.181.0/testing/asserts.ts';
+export { assert, assertEquals } from 'https://deno.land/std@0.181.0/testing/asserts.ts';
 export { Keydb } from 'https://deno.land/x/keydb@1.0.0/sqlite.ts';
---
+++ src/policies/anti-duplication-policy.test.ts
@@ -0,0 +1,28 @@
+import { assertEquals } from '../deps.ts';
+import { buildEvent, buildInputMessage } from '../test.ts';
+
+import antiDuplicationPolicy from './anti-duplication-policy.ts';
+
+Deno.test({
+  name: 'blocks events that post the same content too quickly',
+  fn: async () => {
+    const content = 'Spicy peppermint apricot mediterranean ginger carrot spiced juice edamame hummus';
+
+    const opts = {
+      databaseUrl: `sqlite:///tmp/${crypto.randomUUID()}.sqlite3`,
+    };
+
+    const msg1 = buildInputMessage({ event: buildEvent({ content }) });
+
+    assertEquals((await antiDuplicationPolicy(msg1, opts)).action, 'accept');
+    assertEquals((await antiDuplicationPolicy(msg1, opts)).action, 'shadowReject');
+    assertEquals((await antiDuplicationPolicy(msg1, opts)).action, 'shadowReject');
+
+    const msg2 = buildInputMessage({ event: buildEvent({ content: 'a' }) });
+
+    assertEquals((await antiDuplicationPolicy(msg2, opts)).action, 'accept');
+    assertEquals((await antiDuplicationPolicy(msg2, opts)).action, 'accept');
+    assertEquals((await antiDuplicationPolicy(msg2, opts)).action, 'accept');
+  },
+  sanitizeResources: false,
+});
