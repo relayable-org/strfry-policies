1a83ce44ac76ac2d13a4bbbf0bf2abbc43474f36
Alex Gleason <alex@alexgleason.me>
2023-03-28 16:16:47 -0500

    Rearrange entrypoint.example.ts in a more sensible way

--- entrypoint.example.ts
+++ entrypoint.example.ts
@@ -16,12 +16,12 @@ import {
 for await (const msg of readStdin()) {
   const result = await pipeline(msg, [
     noopPolicy,
-    [hellthreadPolicy, { limit: 100 }],
-    [antiDuplicationPolicy, { ttl: 60000, minLength: 50 }],
-    [rateLimitPolicy, { whitelist: ['127.0.0.1'] }],
-    [pubkeyBanPolicy, ['e810fafa1e89cdf80cced8e013938e87e21b699b24c8570537be92aec4b12c18']],
     [keywordPolicy, ['https://t.me/']],
     [regexPolicy, /(🟠|🔥|😳)ChtaGPT/i],
+    [pubkeyBanPolicy, ['e810fafa1e89cdf80cced8e013938e87e21b699b24c8570537be92aec4b12c18']],
+    [hellthreadPolicy, { limit: 100 }],
+    [rateLimitPolicy, { whitelist: ['127.0.0.1'] }],
+    [antiDuplicationPolicy, { ttl: 60000, minLength: 50 }],
   ]);
 
   writeStdout(result);
