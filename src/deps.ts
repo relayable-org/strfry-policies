33ef127ca7599d9d7016786cbe2de34c9536078c
Alex Gleason <alex@alexgleason.me>
2023-04-17 17:05:23 -0400

    pin esm.sh version with ?pin query, restore deno.lock

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
 export { readLines } from 'https://deno.land/std@0.181.0/io/mod.ts';
 export { assert, assertEquals } from 'https://deno.land/std@0.181.0/testing/asserts.ts';
 export { Keydb } from 'https://raw.githubusercontent.com/alexgleason/Keydb/1bda308df9e589339532daf31f1717ef7a59d2af/sqlite.ts';
-export { type Filter, matchFilter, nip13 } from 'https://esm.sh/nostr-tools@1.8.4';
+export { type Filter, matchFilter, nip13 } from 'https://esm.sh/nostr-tools@1.8.4?pin=v115';
