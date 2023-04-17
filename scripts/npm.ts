7362216da8c1ec5f128dc9a71679bc09275379fd
Alex Gleason <alex@alexgleason.me>
2023-04-16 20:26:32 -0500

    Setup for npm

--- scripts/npm.ts
+++ scripts/npm.ts
@@ -1,4 +1,4 @@
-import { build, emptyDir } from "https://deno.land/x/dnt@0.34.0/mod.ts";
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.34.0/mod.ts';
 
 await emptyDir('./npm');
 
@@ -6,13 +6,13 @@ await build({
   entryPoints: ['./mod.ts'],
   outDir: './npm',
   shims: {
-    // see JS docs for overview and more options
     deno: true,
+    undici: true,
   },
   package: {
     name: 'strfry-policies',
     version: Deno.args[0],
-    description: 'Your package.',
+    description: 'Configurably policies for the strfry Nostr relay.',
     license: 'Unlicense',
     repository: {
       type: 'git',
@@ -22,8 +22,10 @@ await build({
       url: 'https://gitlab.com/soapbox-pub/strfry-policies/-/issues',
     },
   },
+  typeCheck: false,
+  test: false,
+  scriptModule: false,
   postBuild() {
-    // steps to run after building and before running the tests
     Deno.copyFileSync('LICENSE', 'npm/LICENSE');
     Deno.copyFileSync('README.md', 'npm/README.md');
   },
